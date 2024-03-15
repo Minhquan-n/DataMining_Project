a73a19ae2b0cf2455539d819a7df848d9aeda132
Minhquan-n <mquantruong01@gmail.com>
2024-03-16 00:56:50 +0700

    Update api get all information of patient

--- Server/app.py
+++ Server/app.py
@@ -19,12 +19,81 @@ db.close()
 # Cac route
 # Lay danh sach cac benh nhan
 @app.route('/api/patient', methods=['GET'])
-def get_patient_list():
+def get_all_patient():
     patient_list = []
     db = db_connect()
     if db != None:
         cursor = db.cursor()
-        query = 'SELECT p.id, p.fullname, p.current_age, p.address, m.prediction FROM patient_info p LEFT JOIN medical_record m ON p.id = m.patientid'
+        query = 'SELECT p.id, p.fullname, p.current_age, p.address, m.prediction FROM patient_info p LEFT JOIN medical_record m ON p.id = m.patientid ORDER BY m.update_at DESC'
+        cursor.execute(query)
+        for (id, fullname, curent_age, address, prediction) in cursor:
+            patient = {
+                'patient_id': id,
+                'patient_name': fullname,
+                'current_age': curent_age,
+                'address': address,
+                'prediction': prediction
+            }
+            patient_list.append(patient)
+        cursor.close()
+        db.close()
+        return jsonify({'patient_list': patient_list})
+    else:
+        return jsonify({'error': 'Can not get patient list.'})
+    
+@app.route('/api/patient/nonpredict', methods=['GET'])
+def get_all_patient_non_predict():
+    patient_list = []
+    db = db_connect()
+    if db != None:
+        cursor = db.cursor()
+        query = 'SELECT p.id, p.fullname, p.current_age, p.address, m.prediction FROM patient_info p LEFT JOIN medical_record m ON p.id = m.patientid WHERE m.prediction is NULL ORDER BY m.update_at DESC'
+        cursor.execute(query)
+        for (id, fullname, curent_age, address, prediction) in cursor:
+            patient = {
+                'patient_id': id,
+                'patient_name': fullname,
+                'current_age': curent_age,
+                'address': address,
+                'prediction': prediction
+            }
+            patient_list.append(patient)
+        cursor.close()
+        db.close()
+        return jsonify({'patient_list': patient_list})
+    else:
+        return jsonify({'error': 'Can not get patient list.'})
+    
+@app.route('/api/patient/recurrence', methods=['GET'])
+def get_all_patient_recurrence():
+    patient_list = []
+    db = db_connect()
+    if db != None:
+        cursor = db.cursor()
+        query = 'SELECT p.id, p.fullname, p.current_age, p.address, m.prediction FROM patient_info p LEFT JOIN medical_record m ON p.id = m.patientid WHERE m.prediction = "recurrence-events" ORDER BY m.update_at DESC'
+        cursor.execute(query)
+        for (id, fullname, curent_age, address, prediction) in cursor:
+            patient = {
+                'patient_id': id,
+                'patient_name': fullname,
+                'current_age': curent_age,
+                'address': address,
+                'prediction': prediction
+            }
+            patient_list.append(patient)
+        cursor.close()
+        db.close()
+        return jsonify({'patient_list': patient_list})
+    else:
+        return jsonify({'error': 'Can not get patient list.'})
+    
+@app.route('/api/patient/norecurrence', methods=['GET'])
+def get_all_patient_norecurrence():
+    patient_list = []
+    db = db_connect()
+    if db != None:
+        cursor = db.cursor()
+        query = 'SELECT p.id, p.fullname, p.current_age, p.address, m.prediction FROM patient_info p LEFT JOIN medical_record m ON p.id = m.patientid WHERE m.prediction = "no-recurrence-events" ORDER BY m.update_at DESC'
         cursor.execute(query)
         for (id, fullname, curent_age, address, prediction) in cursor:
             patient = {
@@ -79,7 +148,7 @@ def get_patient_detail(id):
     db = db_connect()
     if db != None:
         cursor = db.cursor()
-        select_col = 'p.id, p.fullname, p.date_of_birth, p.current_age, p.address, m.age, m.menopause, m.tumor_size, m.inv_node, m.node_caps, m.deg_malig, m.breast, m.breast_quad, m.irradiar, m.prediction, m.medical_record_id'
+        select_col = 'p.id, p.fullname, p.date_of_birth, p.current_age, p.address, m.age, m.menopause, m.tumor_size, m.inv_node, m.node_caps, m.deg_malig, m.breast, m.breast_quad, m.irradiar, m.prediction, m.medical_record_id, p.phone'
         query = f'SELECT {select_col} FROM patient_info p LEFT JOIN medical_record m ON p.id = m.patientid WHERE p.id = {id}'
         cursor.execute(query)
         for item in cursor:
@@ -99,7 +168,8 @@ def get_patient_detail(id):
                 'breast_quad': item[12],
                 'irradiat': item[13],
                 'prediction': item[14],
-                'medical_record_id': item[15]
+                'medical_record_id': item[15],
+                'phone': item[16]
             }
             response['patient_info'] = info
     return jsonify(response)
@@ -142,8 +212,7 @@ def predict():
     data = preprocess_data_input(input)
     predictions = get_class_value(model.predict([data]))
     response = {
-        'predictions': predictions,
-        'input': input
+        'prediction': predictions
     }
     return jsonify(response)
 
